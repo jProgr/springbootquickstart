cfc6f5cff9a5b94370d4e5fd9a9f1d70c558b38f
jProgr <8537691@gmail.com>
2021-03-03 16:58:06 -0600

    Initial commit

---
+++ src/test/java/jprogr/springbootquickstart/SpringbootquickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package jprogr.springbootquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootquickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
