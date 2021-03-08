ac85ce8f3621627bdabeac3045f1df4bd18f8dfe
jProgr <8537691@gmail.com>
2021-03-08 16:34:44 -0600

    Handling POST requests

--- src/main/java/jprogr/springbootquickstart/controllers/TopicController.java
+++ src/main/java/jprogr/springbootquickstart/controllers/TopicController.java
@@ -2,7 +2,9 @@ package jprogr.springbootquickstart.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import jprogr.springbootquickstart.entities.Topic;
 import jprogr.springbootquickstart.services.TopicService;
@@ -24,5 +26,11 @@ public class TopicController
     public Topic getTopic(@PathVariable String id)
     {
         return topicService.getById(id);
+    }
+
+    @PostMapping("/topics")
+    public void addTopic(@RequestBody Topic topic)
+    {
+        topicService.add(topic);
     }
 }
--- src/main/java/jprogr/springbootquickstart/services/TopicService.java
+++ src/main/java/jprogr/springbootquickstart/services/TopicService.java
@@ -3,7 +3,7 @@ package jprogr.springbootquickstart.services;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-import java.util.Arrays;
+import java.util.ArrayList;
 import jprogr.springbootquickstart.entities.Topic;
 import jprogr.springbootquickstart.util.RandomUtil;
 import javax.annotation.PostConstruct;
@@ -19,10 +19,10 @@ public class TopicService
     @PostConstruct
     private void bootTopics()
     {
-        topics = Arrays.asList(
-            new Topic("a", randomSource.getString(), randomSource.getString()),
-            new Topic("b", randomSource.getString(), randomSource.getString())
-        );
+        topics = new ArrayList();
+
+        topics.add(new Topic("a", randomSource.getString(), randomSource.getString()));
+        topics.add(new Topic("b", randomSource.getString(), randomSource.getString()));
     }
 
     public List<Topic> getAllTopics()
@@ -37,5 +37,10 @@ public class TopicService
             .filter(t -> t.getId().equals(id))
             .findFirst()
             .get();
+    }
+
+    public void add(Topic topic)
+    {
+        topics.add(topic);
     }
 }
